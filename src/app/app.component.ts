712396282a7f06cb7fcde85c9377ec05f27f715b
Gogita Mezurnishvili <m.gogita@gmail.com>
2017-11-21 17:00:10 +0400

    app init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/combineLatest';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  all: any[];
+  greater: any;
+  less: any;
+  inp: string;
+  limit: any;
+  s1: Subject<any>;
+  s2: Subject<any>;
+
+  constructor() {
+      this.s1 = new Subject<any>();
+      this.s2 = new Subject<any>();
+      this.all = [];
+      this.greater = [];
+      this.less = [];
+      this.limit = 3;
+      this.inp = '';
+  }
+
+  ngOnInit() {
+      this.s1.subscribe((data) => {
+          this.all.push(data);
+      });
+      this.s1.filter(val => val.length > this.limit).subscribe((data) => {
+          this.greater.push(data);
+      });
+      this.s1.filter(val => val.length < this.limit).subscribe((data) => {
+          this.less.push(data);
+      });
+  }
+
+  addWord() {
+      if (this.inp === '') { return; }
+      this.s1.next(this.inp);
+      this.inp = '';
+  }
+
 }
